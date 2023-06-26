00db9d93e5f170c8078810087314a32b65eeeb9b
Joshua Lazar <interlink@Joshua-Lazar.local>
2023-06-26 14:31:17 -0700

    spread

--- src/Modern-JavaScript/spread-rest-operator/index.js
+++ src/Modern-JavaScript/spread-rest-operator/index.js
@@ -1,5 +1,6 @@
 
 // ----------------------Rest Operator--------------------------------//
+// Rest operator, denoted by three consecutive dots (...), is a syntax introduced in ECMAScript 6 (ES6) that allows you to represent an indefinite number of function arguments as an array. It is used in function declarations and function expressions to capture multiple arguments into a single parameter.
 /**
  * displayInfo - A function that displays the name and details of a person.
  * 
@@ -28,6 +29,21 @@ const combined = [...fruits, ...vegetables];
 console.log(combined); 
 // Output: ['apple', 'banana', 'orange', 'carrot', 'broccoli']
 
+// rest Operator used in Object
+const originalObject = {
+    username: 'Joshua',
+    age: 22,
+    city: 'Lahore',
+    country: 'Pakistan',
+  };
+  
+  // Extracting specific properties from the original object
+  const { username, age, ...rest } = originalObject;
+  
+  console.log(username); // Output: Joshua
+  console.log(age); // Output: 30
+  console.log(rest); // Output: { city: 'New York', country: 'USA' }
+  
   
 //   --------------------------End---------------------------------------------------
   
@@ -36,11 +52,15 @@ console.log(combined);
 
 // The spread operator, denoted by three consecutive dots (...), is a syntax introduced in ECMAScript 6 (ES6) that allows an iterable (such as an array or string) to be expanded into individual elements. It is primarily used to make copies of arrays, concatenate arrays, and pass multiple arguments to functions.
 
+
+
 // 1 -// Copying Arrays:
 // You can use the spread operator to create a copy of an existing array.
 const originalArray = [1, 2, 3];
 const copiedArray = [...originalArray];
 console.log(copiedArray); // Output: [1, 2, 3]
+
+
 
 
 // 2 Concatenating Arrays:
@@ -49,6 +69,8 @@ const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 const concatenatedArray = [...array1, ...array2];
 console.log(concatenatedArray); // Output: [1, 2, 3, 4, 5, 6]
+
+
 
 // Expanding Strings:
 // In addition to arrays, the spread operator can also be used with strings to split them into individual characters.
@@ -59,3 +81,27 @@ console.log(characters); // Output: ['H', 'e', 'l', 'l', 'o']
 
 
 
+
+// Spread operator used in object 
+const obj1 = { name: 'joshua lazar', age: 22 };
+const obj2 = { city: 'Lahore', country: 'Pakistan' };
+
+
+
+// Creating a shallow copy of an object
+const copiedObj = { ...obj1 };
+console.log(copiedObj); // Output: { name: 'joshua lazar', age: 22 }
+
+
+
+// Merging multiple objects
+const mergedObj = { ...obj1, ...obj2 };
+console.log(mergedObj); // Output: { name: 'joshua lazar', age: 22, city: 'Lahore', country: 'Pakistan' }
+
+
+
+
+const person = { name: 'Joshua', age: 20 };
+// Updating the value of the 'age' property
+const updatedPerson = { ...person, age: 2 };
+console.log(updatedPerson); // Output: { name: 'John', age: 23 }
